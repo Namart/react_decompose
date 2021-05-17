4f39f0621fc8955b3318c5469bd45930ff07378b
Anastasiya <anastasiya.triumf@gmail.com>
2021-05-17 16:06:04 +0300

    Updates

---
+++ precache-manifest.e9b35722f02be0ee2a0e920eca920db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc368dbb9085bbd3dafc500570952eb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1eafe2b35eaf0fa01441",
+    "url": "./static/css/main.5ebd9337.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eafe2b35eaf0fa01441",
+    "url": "./static/js/main.3f065a5f.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
